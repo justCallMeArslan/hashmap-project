aea1b6544477e30567fa97ced5876c8a583c0697
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-06 20:03:19 +0300

    Complete set

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function HashMap(loadFactor = 0.75) {
-
     let capacity = loadFactor === 0.75 ? 16 : 0; // if lF = 0.75 , cap = 16
+    const buckets = [...Array(capacity)].map(() => []); // creating new array for storage
+    // and fill each of slot as separate empty array
+
+
+    function getBucketsForTest() {
+        return buckets;
+    }
 
     function hash(key) {
         let hashCode = 0;
@@ -13,14 +19,33 @@ function HashMap(loadFactor = 0.75) {
     }
 
     function set(key, value) {
-        
+        const index = hash(key);
+        const bucket = buckets[index]; // empty array inside buckets array with 
+        // index of hashCOde
+
+        for (let pair of bucket) {
+            if (pair[0] === key) { // pair split by bey and value, so pair[0] is key
+                pair[1] = value; // update value
+                return;
+            }
+        }
+
+        bucket.push([key, value]);
     }
 
     return {
         loadFactor,
-        hash
+        hash,
+        set,
+        getBucketsForTest
     }
 }
 
-const check = HashMap();
-console.log(check.hash("Arlecchino"));
+const test = HashMap();
+console.log(test.hash("Arlecchino"));
+
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+
+console.log(test.getBucketsForTest());
