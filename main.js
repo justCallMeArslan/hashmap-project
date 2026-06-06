c66bb5fce90e7c304ea79274efcca4fe26211329
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-06 18:11:28 +0300

    Complete and test hash()

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-function HahsMap(loadFactor = 0.75) {
+function HashMap(loadFactor = 0.75) {
 
-    let capacity = loadFactor === 0.75 ? 16 : 0;
+    let capacity = loadFactor === 0.75 ? 16 : 0; // if lF = 0.75 , cap = 16
+
+    function hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+        }
+        return hashCode;
+    }
+
+    function set(key, value) {
+        
+    }
 
     return {
         loadFactor,
-        capacity
+        hash
     }
 }
+
+const check = HashMap();
+console.log(check.hash("Arlecchino"));
